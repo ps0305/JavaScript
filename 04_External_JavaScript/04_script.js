bcd2d284b94a9c4a6e6fe6978764845cce0f5b70
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-29 14:05:02 +0530

    4th commit

---
+++ 04_External_JavaScript/04_script.js
@@ -0,0 +1,14 @@
+// Add your JavaScript code here
+
+    function blue() {
+        document.getElementById('green-p').style.color='blue';
+    }
+    function red() {
+        document.getElementById('green-p').style.color = 'red';
+    }
+    function facebook() {
+        document.getElementById('myImage').setAttribute('src','../img/facebook.jpg');
+    }
+    function youtube() {
+        document.getElementById('myImage').setAttribute('src','../img/youtube.jpg');
+    }
