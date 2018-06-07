c7509a0e710137020d8d5ef61f4280b99a5de5e8
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-06-07 14:23:41 +0530

    commit

---
+++ 00_HTML_CSS_JS/script.js
@@ -0,0 +1,15 @@
+
+
+    function blue() {
+        document.getElementById('blue').setAttribute('src','blue.css');
+    }
+    function red() {
+        document.getElementById('green').setAttribute('src',green.css);
+    }
+    function green() {
+        document.getElementById('red').setAttribute('src',red.css);
+    }
+   // function yellow() {
+        //document.body.setAttribute('href' , yellow());
+   // }
+
