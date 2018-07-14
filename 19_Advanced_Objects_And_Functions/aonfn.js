bffa7523940659b9fe2949ef2988f6adf221190d
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-14 15:36:41 +0530

    commit

--- 19_Advanced_Objects_And_Functions/aonfn.js
+++ 19_Advanced_Objects_And_Functions/aonfn.js
@@ -1,16 +1,27 @@
-/*const Person = function (fisrtName,lastName,age) {
+const Person = function (fisrtName,lastName,age) {
 	this.fisrtName = fisrtName;
 	this.lastName = lastName;
 	this.age = age;
 };
+Person.prototype.getBio = function(){
+	return `${this.fisrtName} is ${this.age}.`
+}
+Person.prototype.setName = function(fullName){
+	const names = fullName.split(' ');
+	this.fisrtName = names[0];
+	this.lastName = names[1];
+}
 
 const person1 = new Person ('Tom','Clanchy',27);
-console.log(person1);*/
+person1.setName('John Doe');
+console.log(person1.getBio());
 
-const Hangman = function(word,attempsts){
+/*const Hangman = function(word,attempsts){
 	this.word = word;
 	this.attempsts = attempsts;
 }
 
+
+
 const game1 = new Hangman('Cat',2);
-const game2 = new Hangman('New Jersey',4);
+const game2 = new Hangman('New Jersey',4);*/
