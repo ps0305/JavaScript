77aec66d5ff44062c18228359f34d8be771e1663
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-14 22:56:04 +0530

    logic

--- Hangman Challenge/Hangman.js
+++ Hangman Challenge/Hangman.js
@@ -2,7 +2,7 @@
 const Hangman = function(word,attempsts){
 	this.word = word.toLowerCase().split('');
 	this.attempsts = attempsts;
-	this.guessedLetters = ['c','t']
+	this.guessedLetters = []
 }
 
 Hangman.prototype.getPuzzle = function(){
@@ -17,8 +17,29 @@ Hangman.prototype.getPuzzle = function(){
 	})
 	return puzzle;
 }
+//Creating Method
+Hangman.prototype.makeGuess = function(guess) {
+	guess = guess.toLowerCase()
+	const isUnique = !this.guessedLetters.includes(guess)
+	const isBadGuess = !this.word.includes(guess)
 
+	if(isUnique){
+		this.guessedLetters.push(guess)
+	}
+	if(isUnique && isBadGuess){
+		this.attempsts--;
+	}
+}
 const game1 = new Hangman('Cat',2);
+//make a guess
+game1.makeGuess('c')
+game1.makeGuess('z')
+game1.makeGuess('t')
 console.log(game1.getPuzzle())
+console.log(game1.attempsts)
+
+//print remaining guesses (should be 1)
 const game2 = new Hangman('New Jersey',4);
-console.log(game2.getPuzzle())
+//guess w
+game2.makeGuess('w')
+console.log(game2.getPuzzle()) //**w ******
