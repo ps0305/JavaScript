b675f16471bbae345d1e95d4fc7a3b34a06b9501
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-14 22:16:44 +0530

    developing

--- 19_Advanced_Objects_And_Functions/Hangman.js
+++ 19_Advanced_Objects_And_Functions/Hangman.js
@@ -1,10 +1,22 @@
-
+  
 const Hangman = function(word,attempsts){
 	this.word = word;
 	this.attempsts = attempsts;
+	this.guessedLetters = ['c','t']
 }
 
+Hangman.prototype.getPuzzle = function(){
+	let puzzle = '';
 
+	this.word.forEach((letter) =>{
+		if(this.guessedLetters.includes(letter) || letter ===''){
+			puzzle += '*';
+		}
+	})
+	return puzzle;
+}
 
 const game1 = new Hangman('Cat',2);
+console.log(game1.getPuzzle())
 const game2 = new Hangman('New Jersey',4);
+console.log(game2.getpuzzle());
