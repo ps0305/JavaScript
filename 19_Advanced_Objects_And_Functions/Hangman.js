2175cfcf3408396c74642ba22b4ed544ed022a1f
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-14 15:48:20 +0530

    seperated

---
+++ 19_Advanced_Objects_And_Functions/Hangman.js
@@ -0,0 +1,10 @@
+
+const Hangman = function(word,attempsts){
+	this.word = word;
+	this.attempsts = attempsts;
+}
+
+
+
+const game1 = new Hangman('Cat',2);
+const game2 = new Hangman('New Jersey',4);
--- 19_Advanced_Objects_And_Functions/person.js
+++ 19_Advanced_Objects_And_Functions/person.js
@@ -15,13 +15,3 @@ Person.prototype.setName = function(fullName){
 const person1 = new Person ('Tom','Clanchy',27);
 person1.setName('John Doe');
 console.log(person1.getBio());
-
-/*const Hangman = function(word,attempsts){
-	this.word = word;
-	this.attempsts = attempsts;
-}
-
-
-
-const game1 = new Hangman('Cat',2);
-const game2 = new Hangman('New Jersey',4);*/
